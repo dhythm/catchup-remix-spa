c55cf3687fae66948d2f530c26ff8c1a647708ad
dhythm <yuta.okada.20@gmail.com>
2024-10-13 01:51:38 +0900

    implement i18n and translate to ja

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,19 +9,52 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-const PokemonListSchema = v.object({ name: v.string(), url: v.string() });
-type PokemonListSchemaType = v.InferOutput<typeof PokemonListSchema>;
+type Types = { en: string; ja: string }[];
+
+const ListSchema = v.object({ name: v.string(), url: v.string() });
+type ListSchemaType = v.InferOutput<typeof ListSchema>;
 
 export default function Index() {
-  const [pokemons, setPokemons] = useState<PokemonListSchemaType[]>([]);
+  const [pokemons, setPokemons] = useState<ListSchemaType[]>([]);
+  const [types, setTypes] = useState<{ en: string; ja: string }[]>([]);
+
   const getAllPokemons = async () => {
     // The number of current pokemons is 1302
     const res = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=50");
-    return v.parse(v.array(PokemonListSchema), (await res.json()).results);
+    return v.parse(v.array(ListSchema), (await res.json()).results);
+  };
+  const getAllTypes = async () => {
+    const res = await fetch("https://pokeapi.co/api/v2/type");
+    return v.parse(v.array(ListSchema), (await res.json()).results);
   };
 
   useEffect(() => {
     getAllPokemons().then((data) => setPokemons(data));
+    getAllTypes().then(async (data) => {
+      const _types = await Promise.all(
+        data.map(async (d) => {
+          const res = await fetch(d.url);
+          return v.parse(
+            v.object({
+              name: v.string(),
+              names: v.array(
+                v.object({
+                  name: v.string(),
+                  language: v.object({ name: v.string(), url: v.string() }),
+                })
+              ),
+            }),
+            await res.json()
+          );
+        })
+      );
+      setTypes(
+        _types.map((d) => ({
+          en: d.name,
+          ja: d.names.find((n) => n.language.name === "ja")?.name || "",
+        }))
+      );
+    });
   }, []);
 
   return (
@@ -46,7 +79,12 @@ export default function Index() {
         </header>
         <div className="flex flex-wrap">
           {pokemons.map((pokemon) => (
-            <PokemonCard key={pokemon.name} url={pokemon.url} />
+            <PokemonCard
+              key={pokemon.name}
+              name={pokemon.name}
+              url={pokemon.url}
+              types={types}
+            />
           ))}
         </div>
       </div>
@@ -97,16 +135,95 @@ const PokemonSchema = v.object({
   weight: v.number(),
 });
 type PokemonSchemaType = v.InferOutput<typeof PokemonSchema>;
-const PokemonCard = ({ url }: { url: string }) => {
+const PokemonSpeciesSchema = v.object({
+  base_happiness: v.number(),
+  capture_rate: v.number(),
+  color: v.any(),
+  egg_groups: v.array(v.any()),
+  evolution_chain: v.any(),
+  evolves_from_species: v.any(),
+  flavor_text_entries: v.array(v.any()),
+  form_descriptions: v.array(v.any()),
+  forms_switchable: v.boolean(),
+  gender_rate: v.number(),
+  genera: v.array(
+    v.object({
+      genus: v.string(),
+      language: v.object({
+        name: v.string(),
+        url: v.string(),
+      }),
+    })
+  ),
+  generation: v.any(),
+  growth_rate: v.any(),
+  habitat: v.any(),
+  has_gender_differences: v.boolean(),
+  hatch_counter: v.number(),
+  id: v.number(),
+  is_baby: v.boolean(),
+  is_legendary: v.boolean(),
+  is_mythical: v.boolean(),
+  name: v.string(),
+  names: v.array(
+    v.object({
+      language: v.object({
+        name: v.string(),
+        url: v.string(),
+      }),
+      name: v.string(),
+    })
+  ),
+  order: v.number(),
+  pal_park_encounters: v.array(v.any()),
+  pokedex_numbers: v.array(
+    v.object({
+      entry_number: v.number(),
+      pokedex: v.object({
+        name: v.string(),
+        url: v.string(),
+      }),
+    })
+  ),
+  shape: v.any(),
+  varieties: v.array(v.any()),
+});
+
+const PokemonCard = ({
+  name,
+  url,
+  types,
+}: {
+  name: string;
+  url: string;
+  types: Types;
+}) => {
   const [pokemon, setPokemon] = useState<PokemonSchemaType>();
   const getPokemon = useCallback(async () => {
     try {
-      const res = await fetch(url);
-      return v.parse(PokemonSchema, await res.json());
+      const resPokemon = await fetch(url);
+      const resSpecies = await fetch(
+        `https://pokeapi.co/api/v2/pokemon-species/${name}`
+      );
+      const pokemonData = v.parse(PokemonSchema, await resPokemon.json());
+      const species = v.parse(PokemonSpeciesSchema, await resSpecies.json());
+      return {
+        ...pokemonData,
+        name:
+          species.names.find((n) => n.language.name === "ja")?.name ||
+          pokemonData.name,
+        types: pokemonData.types.map((t) => ({
+          ...t,
+          type: {
+            ...t.type,
+            name: types.find((_t) => _t.en === t.type.name)?.ja || t.type.name,
+          },
+        })),
+      };
     } catch {
       return;
     }
-  }, [url]);
+  }, [name, types, url]);
 
   useEffect(() => {
     getPokemon().then((data) => setPokemon(data));
@@ -123,7 +240,7 @@ const PokemonCard = ({ url }: { url: string }) => {
       <div className="detail-wrapper">
         <h4>{pokemon.name}</h4>
         <p className="text-sm">
-          {pokemon.types.map((data) => data.type.name).join(", ")}
+          {pokemon.types.map((data) => data.type.name).join("/")}
         </p>
       </div>
     </div>
